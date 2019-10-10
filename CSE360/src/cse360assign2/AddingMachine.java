f01fee37808fe050524fd5fbeac6e83707a9f1bc
Wei <Wei@Galen-pc>
2019-10-09 19:59:42 -0700

    initial commit

---
+++ CSE360/src/cse360assign2/AddingMachine.java
@@ -0,0 +1,51 @@
+package cse360assign2;
+
+public class AddingMachine {
+
+	private int total;
+	
+	/**
+	 * 
+	 * Constructors
+	 * total is initialized as 0.
+	 */
+	public AddingMachine () {
+		total = 0;  // not needed - included for clarity
+	}
+	
+	/**
+	 * @return total
+	 */
+	public int getTotal () {
+		return 0;
+	}
+	
+	/**
+	 * @param value add value with total and store in total
+	 */
+	public void add (int value) {
+		
+	}
+	
+	/**
+	 * @param value subtract value from total
+	 */
+	public void subtract (int value) {
+		
+	}
+	
+	/**
+	 * @see java.lang.Object#toString()
+	 * @return the history of transactions
+	 */
+	public String toString () {
+		return "";
+	}
+	
+	/**
+	 * Reset history and total.
+	 */
+	public void clear() {
+	
+	}
+}
