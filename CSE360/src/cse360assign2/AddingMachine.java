3c94f0038a5c295331d9f39a9c3ce72fc4ed6b71
Wei <Wei@Galen-pc>
2019-10-09 20:19:16 -0700

    added code for methods

--- CSE360/src/cse360assign2/AddingMachine.java
+++ CSE360/src/cse360assign2/AddingMachine.java
@@ -3,6 +3,7 @@ package cse360assign2;
 public class AddingMachine {
 
 	private int total;
+	private String history;
 	
 	/**
 	 * 
@@ -10,28 +11,31 @@ public class AddingMachine {
 	 * total is initialized as 0.
 	 */
 	public AddingMachine () {
-		total = 0;  // not needed - included for clarity
+		total = 0;  
+		history = "0";
 	}
 	
 	/**
 	 * @return total
 	 */
 	public int getTotal () {
-		return 0;
+		return total;
 	}
 	
 	/**
 	 * @param value add value with total and store in total
 	 */
 	public void add (int value) {
-		
+		total += value;
+		history += " + " + value;
 	}
 	
 	/**
 	 * @param value subtract value from total
 	 */
 	public void subtract (int value) {
-		
+		total -= value;
+		history += " - " + value;
 	}
 	
 	/**
@@ -39,13 +43,14 @@ public class AddingMachine {
 	 * @return the history of transactions
 	 */
 	public String toString () {
-		return "";
+		return history;
 	}
 	
 	/**
 	 * Reset history and total.
 	 */
 	public void clear() {
-	
+		total =0;
+		history ="0";
 	}
 }
